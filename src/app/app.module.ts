e9d5245e116b3cfd300c4f951b822b3dbd325e95
chris <ghinzu89@gmail.com>
2017-11-04 12:07:51 +0100

    api nouvelle page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,9 @@ import { MyApp } from './app.component';
 // Pages
 import { HomePage } from '../pages/home/home';
 import { DetailsPage } from '../pages/details/details';
-<<<<<<< HEAD
 import { NewsapiPage } from '../pages/newsapi/newsapi';
 
 //Service
-=======
->>>>>>> 1e0b4fbe1b40beff1838dedd51f58d9c0d81bad1
 import { NewsApiService } from '../services/newsapi.service'
 
 
@@ -24,11 +21,8 @@ import { NewsApiService } from '../services/newsapi.service'
     MyApp,
     HomePage,
     DetailsPage,
-<<<<<<< HEAD
     NewsapiPage,
 
-=======
->>>>>>> 1e0b4fbe1b40beff1838dedd51f58d9c0d81bad1
   ],
   imports: [
     
@@ -41,10 +35,7 @@ import { NewsApiService } from '../services/newsapi.service'
     MyApp,
     HomePage,
     DetailsPage,
-<<<<<<< HEAD
     NewsapiPage,
-=======
->>>>>>> 1e0b4fbe1b40beff1838dedd51f58d9c0d81bad1
   ],
   providers: [
     NewsApiService,
